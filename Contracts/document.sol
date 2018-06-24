88238b540d2be88384c6e1af2af6b34c9c54e2a0
unknown <owen.a.kane@gmail.com>
2018-06-24 20:15:40 +0100

    Update to contract and abi in JS

--- Contracts/document.sol
+++ Contracts/document.sol
@@ -3,24 +3,27 @@ pragma solidity ^0.4.0;
 contract document {
 
     struct docData{
-        bytes32 docText;
+        string docText;
         bool isEncrypted;
     }
 
     docData[] public docs;
 
     mapping (uint => address) public docToOwner;
-    mapping (address => uint) ownerDocCount;
+    mapping (address => uint[]) public ownerToDocsIds;
 
-    function _getDoc(uint _docId) view public returns (bytes32){
-
+    function _getDoc(uint _docId) view public returns (string){
         return docs[_docId].docText;
     }
 
-    function _storeDocument(bytes32 _docText, bool _isEncrypted) public returns (uint) {
+    function _storeDocument(string _docText, bool _isEncrypted) public returns (uint) {
         uint id = docs.push(docData(_docText, _isEncrypted)) - 1;
         docToOwner[id] = msg.sender;
-        ownerDocCount[msg.sender]++;
+        ownerToDocsIds[msg.sender].push(id);
         return id;
+    }
+
+    function _getDocIDs(address _userAddr) view public returns (uint[]){
+        return ownerToDocsIds[_userAddr];
     }
 }
--- Docs_On_The_Blockchain/static/getDoc.js
+++ Docs_On_The_Blockchain/static/getDoc.js
@@ -2,92 +2,134 @@ const Web3 = require('web3');
 let accounts;
 let web3;
 
-const address = '0x159761E018B920Ca930f71e86E1Ba86eAE818C4F';
+const address = '0xf4BBF8365ACAE4dbe0b131c5BE3d8297A70dA573';
 const abi = [
-  {
-    "constant": false,
-    "inputs": [
-      {
-        "name": "_docText",
-        "type": "bytes32"
-      },
-      {
-        "name": "_isEncrypted",
-        "type": "bool"
-      }
-    ],
-    "name": "_storeDocument",
-    "outputs": [
-      {
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "_docId",
-        "type": "uint256"
-      }
-    ],
-    "name": "_getDoc",
-    "outputs": [
-      {
-        "name": "",
-        "type": "bytes32"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "docs",
-    "outputs": [
-      {
-        "name": "docText",
-        "type": "bytes32"
-      },
-      {
-        "name": "isEncrypted",
-        "type": "bool"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "docToOwner",
-    "outputs": [
-      {
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "docs",
+		"outputs": [
+			{
+				"name": "docText",
+				"type": "string"
+			},
+			{
+				"name": "isEncrypted",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_docText",
+				"type": "string"
+			},
+			{
+				"name": "_isEncrypted",
+				"type": "bool"
+			}
+		],
+		"name": "_storeDocument",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "ownerToDocsIds",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_docId",
+				"type": "uint256"
+			}
+		],
+		"name": "_getDoc",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "docToOwner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_userAddr",
+				"type": "address"
+			}
+		],
+		"name": "_getDocIDs",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
 
 if (typeof window.web3 !== 'undefined') {
